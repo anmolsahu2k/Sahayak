ddee9ac3f2e8d9a3c31904df2b37ab5c56d370b9
Pro-Abhinav <abhinav16032002@gmail.com>
2021-02-24 01:01:13 +0530

    [INIT] initial commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,87 @@
+const   express = require("express"),
+        router = express.Router(),
+        passport = require("passport");
+
+const User = require("../models/userModel");
+
+
+
+// -------------Login GET request--------------//
+router.get("/userLogin", function (req, res) {
+  // console.log(req.flash(error));
+  res.render("auth/login");
+});
+
+
+//---------------Login POST request-------------//
+router.post(
+  "/userLogin",
+  passport.authenticate("local", {
+    failureRedirect: "/userlogin",
+    failureFlash: true,
+  }),
+  function (req, res) {
+    if (req.user.role == "patient") {
+      req.flash("success", "Welcome to MedBuddy! " + req.user.username);
+      res.redirect("/");
+
+    } else if (req.user.role == "doctor") {
+      req.flash("success", "Welcome to MedBuddy! " + req.user.username);
+      res.redirect("/");
+
+    } else res.send(404);
+  }
+);
+
+//logout
+router.get("/logout", function (req, res) {
+  req.logout();
+  req.flash("success", "Logged Out Successfully! ");
+  res.redirect("/");
+});
+
+//Signup get requests
+router.get("/register", function (req, res) {
+  res.render("auth/signup");
+});
+
+//Signup post request
+router.post("/register", function (req, res) {
+  // console.log("post in /register");
+  var newUser = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    address: {
+      street: req.body.streetAddress,
+      city: req.body.city,
+      state: req.body.state,
+      zip: req.body.zip,
+    },
+    gender: req.body.gender,
+    username: req.body.username,
+    email: req.body.email,
+    phone: req.body.contactNumber,
+    avatar: req.body.avatar,
+    role: req.body.userRole,
+  });
+  User.register(newUser, req.body.password, function (err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        req.flash("success", "Welcome to MedBuddy " + user.username);
+        if (req.user.role == "patient")
+          res.redirect("/");
+        else if (req.user.role == "doctor")
+          res.redirect("/");
+        else if (req.user.role == "hospAdmin")
+          res.redirect("/");
+        else res.send(404);
+      });
+    }
+  });
+});
+module.exports = router;
+
+
