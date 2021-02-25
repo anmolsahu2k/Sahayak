b7fbcbc1f1bc893bf67d3b62d020549232fffb81
Anmol Sahu <anmolsahu2k@gmail.com>
2021-02-25 11:07:00 +0530

    [MODIFY] Add settings route

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -10,4 +10,8 @@ router.get("/dashboard", function(req, res) {
     res.render("dashboard");
 });
 
+router.get("/settings", function(req, res) {
+    res.render("settings");
+});
+
 module.exports = router;
