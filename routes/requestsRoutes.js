0ef5ed64034a1519f65d6e490b6882969a8ab2da
Anmol Sahu <anmolsahu2k@gmail.com>
2021-03-18 15:58:15 +0530

    [MODIFY] get sourceLocation

--- routes/requestsRoutes.js
+++ routes/requestsRoutes.js
@@ -7,66 +7,69 @@ const Request = require("../models/requestSchema");
 const User = require("../models/userModel")
 
 //----------Send Medical SOS reqeust----------//
-router.post("/request/send/medical/:id", middleware.isLoggedIn, function(req,res){
+router.post("/request/send/medical/:id", middleware.isLoggedIn, function(req, res) {
     let newRequest = {
         typeOfRequest: "Medical",
         handler: {
             id: req.params.id,
             username: req.user.username,
         },
-        sourceLocation: String,
+        sourceLocation: {
+            lat: req.body.latitude,
+            long: req.body.longitude
+        },
         message: req.body.message,
     }
-    if(req.user.medicalRequestCount < 3)
-    {
-        Request.create(newRequest, function(err, newRequest){
-            if(err){
+    if (req.user.medicalRequestCount < 3) {
+        Request.create(newRequest, function(err, newRequest) {
+            if (err) {
                 console.log(err);
-            }else{
-                User.findByIdAndUpdate({_id: req.params.id}, {$inc: { medicalRequestCount:1 } },  function(error, updatedUser){
-                    if(error){
+            } else {
+                User.findByIdAndUpdate({ _id: req.params.id }, { $inc: { medicalRequestCount: 1 } }, function(error, updatedUser) {
+                    if (error) {
                         console.log(error);
-                    }else{
+                    } else {
                         req.flash("success", "Medical SOS Request Sent!");
                         res.redirect("/dashboard");
                     }
                 });
             }
         });
-    } else{
+    } else {
         req.flash("error", "Maximum limit of sending Medical SOS is reached !");
         res.redirect("/dashboard");
     }
+    // console.log(req.body);
 });
 
 //-----------Close request routes------------------//
-router.get("/dashboard/activity/close/:id", function(req, res){
+router.get("/dashboard/activity/close/:id", function(req, res) {
     var datetime = new Date();
-    Request.findByIdAndUpdate(req.params.id, {$set: {'currentStatus': 'Inactive'}}, function(err, updatedRequest){
-            if(err){
-                req.flash("error", "Something went wrong!");
-                console.log(err);
-            }else{
-                closeTime = (datetime-updatedRequest.generatedAt)/(1000*60);
-                let trustScoreChange;
-                if(closeTime < 10){
-                    trustScoreChange = -10;
-                }else if(closeTime < 60 && closeTime > 10){
-                    trustScoreChange = -5;
-                } else if(closeTime < 90 && closeTime > 60){
-                    trustScoreChange = -2;
-                }else {
-                    trustScoreChange = 0;
-                }
-                User.findByIdAndUpdate({_id: req.user._id}, {$inc: { medicalRequestCount:-1, trustScore: trustScoreChange} },  function(error, updatedUser){
-                    if(error){
-                        console.log(error);
-                    }else{
-                        req.flash("success", "SOS Request Closed Successfully!")
-                        res.redirect("/dashboard/activityLog");
-                    }
-                });
+    Request.findByIdAndUpdate(req.params.id, { $set: { 'currentStatus': 'Inactive' } }, function(err, updatedRequest) {
+        if (err) {
+            req.flash("error", "Something went wrong!");
+            console.log(err);
+        } else {
+            closeTime = (datetime - updatedRequest.generatedAt) / (1000 * 60);
+            let trustScoreChange;
+            if (closeTime < 10) {
+                trustScoreChange = -10;
+            } else if (closeTime < 60 && closeTime > 10) {
+                trustScoreChange = -5;
+            } else if (closeTime < 90 && closeTime > 60) {
+                trustScoreChange = -2;
+            } else {
+                trustScoreChange = 0;
             }
+            User.findByIdAndUpdate({ _id: req.user._id }, { $inc: { medicalRequestCount: -1, trustScore: trustScoreChange } }, function(error, updatedUser) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    req.flash("success", "SOS Request Closed Successfully!")
+                    res.redirect("/dashboard/activityLog");
+                }
+            });
+        }
     });
 });
 
