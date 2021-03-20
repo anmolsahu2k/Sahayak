1029fc213892545336d3da68e58a2b3595f990e6
Anmol Sahu <anmolsahu2k@gmail.com>
2021-03-20 15:55:43 +0530

    [MOVE] File moved

--- public/js/dashboard.js
+++
@@ -1,6 +0,0 @@
-var crimeAttributes = $("#alloptions div input[type='checkbox']");
-crimeAttributes.on('change', function() {
-    $('#crimeMsg').val(
-        $.trim($('#crimeMsg').val()) + this.value
-    );
-});
